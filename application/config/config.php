6e9831d3db92c8359e948021e653e9147deb6929
CayoCaesar <julio.uzcategui.excle@gmail.com>
2018-04-30 09:23:48 -0400

    imagen de mantenimiento  y vista

--- application/config/config.php
+++ application/config/config.php
@@ -541,4 +541,4 @@ $config['proxy_ips'] = '';
  | $config['maintenance_mode'] = TRUE; // site is offline
  | $config['maintenance_mode'] = FALSE; // site is online
  */
-$config['maintenance_mode'] = false;
+$config['maintenance_mode'] = true;
